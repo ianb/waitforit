3faef3bb3ffbde6045b7d5bf44c80aa3c8afb897
Ian Bicking <ian@ianbicking.org>
2007-05-26 18:47:43 +0000

    Auto-update of version strings

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '0.1'
+version = '0.2'
 
 index_fn = os.path.join(os.path.dirname(__file__), 'docs', 'index.txt')
 f = open(index_fn)
