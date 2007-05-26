f2a0ffd0750852469db5754993ba263fb4b59003
Ian Bicking <ian@ianbicking.org>
2007-05-26 18:09:32 +0000

    Make it work

--- waitforit/middleware.py
+++ waitforit/middleware.py
@@ -19,6 +19,7 @@ class WaitForIt(object):
                  template=None):
         self.app = app
         self.time_limit = time_limit
+        self.poll_time = poll_time
         self.pending = {}
         if template is None:
             template = TEMPLATE
@@ -28,17 +29,18 @@ class WaitForIt(object):
 
     def __call__(self, environ, start_response):
         path_info = environ.get('PATH_INFO', '')
-        try:
-            id = self.get_id(environ)
-            if id:
-                self.send_wait_page(environ, start_response, id=id)
-        except 0:
-            pass
         if path_info.startswith('/.waitforit/'):
             path_info_pop(environ)
             return self.check_status(environ, start_response)
+        try:
+            id = self.get_id(environ)
+            if id:
+                return self.send_wait_page(environ, start_response, id=id)
+        except KeyError:
+            pass
         data = []
         progress = {}
+        environ['waitforit.progress'] = progress
         event = threading.Event()
         self.launch_application(environ, data, event, progress)
         event.wait(self.time_limit)
@@ -57,11 +59,11 @@ class WaitForIt(object):
         if self.pending[id][0]:
             # Response has come through
             # FIXME: delete cookie
-            data, event = self.pending.pop(id)
+            data, event, progress = self.pending.pop(id)
             return self.send_page(start_response, data)
         request_url = construct_url(environ)
         waitforit_url = construct_url(environ, path_info='/.waitforit/')
-        page = template.substitute(
+        page = self.template.substitute(
             request_url=request_url,
             waitforit_url=waitforit_url,
             poll_time=self.poll_time,
@@ -73,42 +75,51 @@ class WaitForIt(object):
         start_response('200 OK',
                        [('Content-Type', 'text/html; charset=utf8'),
                         ('Content-Length', str(len(page))),
-                        # FIXME: some expire/no-cache header
+                        ('Set-Cookie', 'waitforit_id=%s' % id),
                         ])
         return [page]
 
     def send_page(self, start_response, data):
         status, headers, exc_info, app_iter = data
+        headers.append(
+            ('Set-Cookie', 'waitforit_id=""; expires="Thu, 2 Aug 2001 12:00:00 UTC"'))
         start_response(status, headers, exc_info)
         return app_iter
 
     def get_id(self, environ):
         cookies = get_cookies(environ)
-        id = str(cookie['waitforit_id'])
+        id = cookies['waitforit_id'].value
         return id
 
     def check_status(self, environ, start_response, id=None):
         assert environ['PATH_INFO'] == '/status.json'
         if id is None:
-            id = self.get_id(environ)
+            try:
+                id = self.get_id(environ)
+            except KeyError:
+                body = "There is no pending request with the id %s" % id
+                start_response('400 Bad Request', [
+                    ('Content-type', 'text/plain'),
+                    ('Content-length', str(len(body)))])
+                return [body]
         data, event, progress = self.pending[id]
-        environ['waitforit.progress'] = progress
         if not data:
-            result = {'done': 'false', 'progress': progress}
+            result = {'done': False, 'progress': progress}
         else:
-            result = {'done': 'true'}
+            result = {'done': True}
         start_response('200 OK',
                        [('Content-Type', 'application/json'),
                         ('Content-Length', str(len(result))),
                         ])
         return [simplejson.dumps(result)]
 
-    def launch_application(self, environ, data, event):
+    def launch_application(self, environ, data, event, progress):
         t = threading.Thread(target=self.run_application,
-                             args=(environ, data, event))
+                             args=(environ, data, event, progress))
+        t.setDaemon(True)
         t.start()
 
-    def run_application(self, environ, data, event):
+    def run_application(self, environ, data, event, progress):
         start_response_data = []
         output = []
         def start_response(status, headers, exc_info=None):
@@ -134,9 +145,7 @@ TEMPLATE = '''\
     <<JAVASCRIPT>>
   </script>
   <style type="text/css">
-    body {
-      font-family: sans-serif;
-    }
+    <<CSS>>
   </style>
  </head>
  <body onload="checkStatus()">
@@ -147,10 +156,12 @@ TEMPLATE = '''\
    The page you have requested is taking a while to generate...
  </p>
 
- <p id="progress">
+ <p id="progress-box">
  </p>
 
- <p id="error">
+ <p id="percent-box">
+
+ <p id="error-box">
  </p>
  
  </body>
@@ -163,7 +174,7 @@ function getXMLHttpRequest() {
         function () { return new XMLHttpRequest(); },
         function () { return new ActiveXObject('Msxml2.XMLHTTP'); },
         function () { return new ActiveXObject('Microsoft.XMLHTTP'); },
-        function () { return new ActiveXObject('Msxml2.XMLHTTP.4.0'); },
+        function () { return new ActiveXObject('Msxml2.XMLHTTP.4.0'); }
         ];
     for (var i = 0; i < tryThese.length; i++) {
         var func = tryThese[i];
@@ -183,24 +194,63 @@ function checkStatus() {
         }
     };
     xhr.open("GET", waitforit_url + "status.json");
+    xhr.send(null);
 }
+
+var percent_inner = null;
 
 function statusReceived(req) {
     if (req.status != 200) {
-        var el = document.getElementById("error");
+        var el = document.getElementById("error-box");
         el.innerHTML = req.responseText;
         return;
     }
-    var status = eval(req.responseText);
-    if (status.done) {
-        window.reload();
+    var status = eval("dummy="+req.responseText);
+    if (typeof status.done == "undefined") {
+        // Something went wrong
+        var el = document.getElementById("error-box");
+        el.innerHTML = req.responseText;
+        return;
     }
-    if (progress.message) {
-        var el = document.getElementById("progress");
-        el.innerHTML = progress.message;
+    if (status.done) {
+        window.location.reload();
+        return;
+    }
+    if (status.progress.message) {
+        var el = document.getElementById("progress-box");
+        el.innerHTML = status.progress.message;
+    }
+    if (status.progress.percent) {
+        if (! percent_inner) {
+            var outer = document.createElement("div");
+            outer.setAttribute("id", "percent-container");
+            percent_inner = document.createElement("div");
+            percent_inner.setAttribute("id", "percent-inner");
+            //percent_inner.innerHTML = "&nbsp;";
+            outer.appendChild(percent_inner);
+            var parent = document.getElementById("percent-box");
+            parent.appendChild(outer);
+        }
+        percent_inner.style.width = ""+Math.round(status.progress.percent) + "%";
     }
     setTimeout("checkStatus()", poll_time*1000);
 }
 '''
 
-TEMPLATE = TEMPLATE.replace('<<JAVASCRIPT>>', JAVASCRIPT)
+CSS = '''\
+body {
+  font-family: sans-serif;
+}
+div#percent-container {
+  border: 1px solid #000;
+  width: 100%;
+  height: 20px;
+}
+div#percent-inner {
+  background-color: #999;
+  height: 100%;
+}
+'''
+
+TEMPLATE = TEMPLATE.replace('<<JAVASCRIPT>>', JAVASCRIPT);
+TEMPLATE = TEMPLATE.replace('<<CSS>>', CSS);
